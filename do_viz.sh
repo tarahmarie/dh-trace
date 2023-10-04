dcb31e827abfcce90f233cbd88cde1b231ce74a5
jdmartin <jonsaccount+githubjd@icloud.com>
2023-10-04 14:54:44 -0700

    First pass at jumbo comparison

--- do_viz.sh
+++ do_viz.sh
@@ -30,10 +30,11 @@ menu () {
     tput clear;
     printf "\nWhat kind of visualization would you like to make?\n"
     printf "\t1. Interactive Line Dashboard\n"
-    printf "\t2. Selective Line Graph\n"
-    printf "\t3. Scatterplot\n"
-    printf "\t4. Histogram\n"
-    printf "\t5. Confusion Matrix\n"
+    printf "\t2. All vs. All Dashboard (slow)\n"
+    printf "\t3. Selective Line Graph\n"
+    printf "\t4. Scatterplot\n"
+    printf "\t5. Histogram\n"
+    printf "\t6. Confusion Matrix\n"
     printf "\tQ. Quit\n"
     read -rp "> " viz_choice
 
@@ -42,12 +43,14 @@ menu () {
             start_dash;
             open_dash ;;
         2)
-            python make_lines.py ;;
+            python make_jumbo_dash.py ;;
         3)
-            python make_auto_scatterplot.py ;;
+            python make_lines.py ;;
         4)
-            python make_histogram.py ;;
+            python make_auto_scatterplot.py ;;
         5)
+            python make_histogram.py ;;
+        6)
             python make_confusion.py ;;
         q|Q)
             exit 0 ;;
